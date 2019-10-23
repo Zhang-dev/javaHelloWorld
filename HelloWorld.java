fe28a3220c2f07803a6f68495df382725911a8fc
Zhang-dev <55280516+Zhang-dev@users.noreply.github.com>
2019-10-23 10:30:55 +0200

    first java programm

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String []args) {
+        double[] a = {1.2, 4.3, 3.1, 9.0};
+        for(int i = 0; i < a.length; i++){
+          System.out.println(a[i]);
+        }
+    }
+}
