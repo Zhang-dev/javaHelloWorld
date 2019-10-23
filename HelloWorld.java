d70391d3c430a17d014fd1db18220f751d16e260
Zhang-dev <55280516+Zhang-dev@users.noreply.github.com>
2019-10-23 10:52:45 +0200

    new Branch file, HelloWorld modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,5 @@
 public class HelloWorld {
     public static void main(String []args) {
-        double[] a = {1.2, 4.3, 3.1, 9.0};
-        for(int i = 0; i < a.length; i++){
-          System.out.println(a[i]);
-        }
+        System.out.println("Hello World");
     }
 }
